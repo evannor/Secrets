1dd49adc46e761490cefdd4d72f66bee8866438b
evannor <erica.vannorman@gmail.com>
2020-12-20 17:35:08 -0800

    GET routes

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-
+app.get('/', function(req, res) {
+  res.render('home');
+});
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+app.get('/register', function(req, res) {
+  res.render('register');
+});
 
 app.listen(3000, function() {
   console.log("Server started on Port 3000.");
