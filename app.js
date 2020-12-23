b49ab2e7b9a7d2f4d33ff2c0e486d7f57999fb11
evannor <erica.vannorman@gmail.com>
2020-12-22 17:21:30 -0800

    Hashing password MD5, install bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -21,8 +21,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User = new mongoose.model("User", userSchema);
 
 app.get('/', function(req, res) {
@@ -38,7 +36,7 @@ app.get('/register', function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -58,7 +56,7 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (user) {
-        if (user.password === req.body.password) {
+        if (user.password === md5(req.body.password)) {
           res.render("secrets");
         } else {
           console.log("Incorrect password.");
